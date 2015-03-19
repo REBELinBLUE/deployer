cb32e8270f3eb4e75fbc00d2f3397b8c35e9697f
Stephen Ball <stephen@rebelinblue.com>
2015-03-19 17:14:58 +0000

    Clean up sidebar

--- resources/views/_partials/sidebar.blade.php
+++ resources/views/_partials/sidebar.blade.php
@@ -1,8 +1,5 @@
-<!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
-        <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
                 <img src="{{ asset('/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image" />
@@ -12,7 +9,6 @@
             </div>
         </div>
 
-        <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
             <li class="{{ Request::is('/') ? 'active' : null }}">
                 <a href="/">
@@ -21,7 +17,7 @@
                 </a>
             </li>
 
-            <li class="treeview {{ Request::is('project/*') ? 'active' : null }}">
+            <li class="treeview {{ (Request::is('project/*') OR Request::is('deployment/*')) ? 'active' : null }}">
                 <a href="#">
                     <i class="fa fa-book"></i>
                     <span>Projects</span>
@@ -29,7 +25,7 @@
                 </a>
                 <ul class="treeview-menu">
                     @foreach(App\Project::all() as $project)
-                        <li class="{{ (Request::is('project/' . $project->id) OR Request::is('project/' . $project->id . '/*')) ? 'active' : '' }}"><a href="{{ route('project', $project->id) }}"><i class="fa fa-{{ project_icon_status($project) }} text-{{ project_css_status($project) }}"></i> {{ $project->name }}</a></li>
+                        <li><a href="{{ route('project', $project->id) }}"><i class="fa fa-{{ project_icon_status($project) }} text-{{ project_css_status($project) }}"></i> {{ $project->name }}</a></li>
                     @endforeach
                 </ul>
             </li>
@@ -49,110 +45,6 @@
                     <li><a href="#"><i class="fa fa-circle-o"></i> Modals</a></li>
                 </ul>
             </li>
-
-            <!--li class="treeview">
-                <a href="#">
-                    <i class="fa fa-files-o"></i>
-                    <span>Layout Options</span>
-                    <span class="label label-primary pull-right">4</span>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Boxed</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Fixed</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa fa-th"></i> <span>Widgets</span> <small class="label pull-right bg-green">new</small>
-                </a>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-pie-chart"></i>
-                    <span>Charts</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Morris</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Flot</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Inline charts</a></li>
-                </ul>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-edit"></i> <span>Forms</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> General Elements</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Editors</a></li>
-                </ul>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-table"></i> <span>Tables</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Simple tables</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Data tables</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa fa-calendar"></i> <span>Calendar</span>
-                    <small class="label pull-right bg-red">3</small>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa fa-envelope"></i> <span>Mailbox</span>
-                    <small class="label pull-right bg-yellow">12</small>
-                </a>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-folder"></i> <span>Examples</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Invoice</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Login</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Register</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> 404 Error</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> 500 Error</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Blank Page</a></li>
-                </ul>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-share"></i> <span>Multilevel</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-                    <li>
-                        <a href="#"><i class="fa fa-circle-o"></i> Level One <i class="fa fa-angle-left pull-right"></i></a>
-                        <ul class="treeview-menu">
-                            <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
-                            <li>
-                                <a href="#"><i class="fa fa-circle-o"></i> Level Two <i class="fa fa-angle-left pull-right"></i></a>
-                                <ul class="treeview-menu">
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-                </ul>
-            </li>
-            <li><a href="documentation/index.html"><i class="fa fa-book"></i> Documentation</a></li-->
         </ul>
     </section>
-    <!-- /.sidebar -->
 </aside>
