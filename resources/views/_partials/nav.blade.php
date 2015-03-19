4cc97dcda737c40a96878525fb96a8bbe1583f14
Stephen Ball <stephen@rebelinblue.com>
2015-03-19 17:30:40 +0000

    HTML cleanup

--- resources/views/_partials/nav.blade.php
+++ resources/views/_partials/nav.blade.php
@@ -1,30 +1,23 @@
 <header class="main-header">
-    <!-- Logo -->
     <a href="/" class="logo"><b>Deployer</b></a>
-    <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top" role="navigation">
-        <!-- Sidebar toggle button-->
         <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
         </a>
-        <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-                <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="{{ asset('/dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image"/>
                         <span class="hidden-xs">USERNAME</span>
                     </a>
                     <ul class="dropdown-menu">
-                        <!-- User image -->
                         <li class="user-header">
                             <img src="{{ asset('/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image" />
                             <p>
                                 USERNAME
                             </p>
                         </li>
-                        <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
                                 <a href="#" class="btn btn-default btn-flat">Profile</a>
