779b00cd0ef737db9045033f404d468bcab1f087
Stephen Ball <stephen@rebelinblue.com>
2015-03-25 17:31:26 +0000

    Setting up mail config and making URL configurable

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'deploy@deploy.app', 'name' => 'Deployer'],
 
 	/*
 	|--------------------------------------------------------------------------
