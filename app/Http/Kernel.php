921a8057f642c38dbcd5a0d7478042b424db7fa1
Stephen Ball <stephen@rebelinblue.com>
2015-03-25 13:31:48 +0000

    Removing unused middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,7 +25,6 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => 'App\Http\Middleware\Authenticate',
-        'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
         'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
     ];
 }
