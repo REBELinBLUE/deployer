bcf4b644cdcf29b7359361a92569f6ad6bc1cd4e
Stephen Ball <stephen@aardvarklondon.com>
2015-04-01 17:27:50 +0100

    Clean up

--- app/User.php
+++ app/User.php
@@ -12,13 +12,6 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
     use Authenticatable, CanResetPassword, SoftDeletes;
 
     /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
-
-    /**
      * The attributes that are mass assignable.
      *
      * @var array
