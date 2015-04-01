dc29cd43bae509dffa32e221b45a3951645dd762
Stephen Ball <stephen@aardvarklondon.com>
2015-04-01 15:05:13 +0100

    Removing profile button

--- resources/views/_partials/nav.blade.php
+++ resources/views/_partials/nav.blade.php
@@ -19,9 +19,6 @@
                             </p>
                         </li>
                         <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            </div>
                             <div class="pull-right">
                                 <a href="{{ url('auth/logout') }}" class="btn btn-default btn-flat">Sign out</a>
                             </div>
