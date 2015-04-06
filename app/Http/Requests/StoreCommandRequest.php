600646ff6fbe31929e343632d470512e2100d60f
Stephen Ball <stephen@rebelinblue.com>
2015-04-06 23:16:33 +0100

    Checking project_id on StoreCommandRequest and ensuring step is valid

--- app/Http/Requests/StoreCommandRequest.php
+++ app/Http/Requests/StoreCommandRequest.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Requests;
 
 use Input;
+use App\Command;
 use App\Http\Requests\Request;
 
 /**
@@ -18,9 +19,9 @@ class StoreCommandRequest extends Request
         $rules = [
             'name'       => 'required|max:255',
             'user'       => 'required|max:255',
-            'script'     => 'required|max:255',
-            'step'       => 'required', // FIXME: There are a set of values
-            'project_id' => 'required|integer'
+            'script'     => 'required',
+            'step'       => 'required|integer|min:' . Command::BEFORE_CLONE . '|max:' . Command::AFTER_PURGE,
+            'project_id' => 'required|integer|exists:projects,id'
         ];
 
         // On edit we don't require the step or the project_id
