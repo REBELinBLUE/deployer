e30fd39261b7fdeb2947c619b93d205b3ad585ea
Stephen Ball <stephen@aardvarklondon.com>
2015-04-08 17:21:13 +0100

    Making mail from configurable

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => 'deploy@deploy.app', 'name' => 'Deployer'],
+    'from' => ['address' => env('MAIL_FROM_ADDRESS', 'deploy@deploy.app'), 'name' => env('MAIL_FROM_NAME', 'Deployer')],
 
     /*
     |--------------------------------------------------------------------------
