1d62a639a9d0e3132ae68b6395de10a9217d436a
Mike Hughes <mike@aardvarklondon.com>
2015-04-12 01:23:52 +0100

    changed key dialog to not escape Copy that contains html

--- resources/views/dialogs/key.blade.php
+++ resources/views/dialogs/key.blade.php
@@ -3,18 +3,18 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                <h4 class="modal-title"><i class="fa fa-key"></i> {!! Lang::get('projects.public_ssh_key') !!}</h4>
+                <h4 class="modal-title"><i class="fa fa-key"></i> {{{}} Lang::get('projects.public_ssh_key') }</h4>
             </div>
             <div class="modal-body">
                 <div class="alert alert-warning">
-                    <p>{{ Lang::get('projects.server_keys') }}</p>
-                    <p>{{ Lang::get('projects.gitlab_keys') }}</p>
+                    <p>{!! Lang::get('projects.server_keys') !!}</p>
+                    <p>{!! Lang::get('projects.gitlab_keys') !!}</p>
                 </div>
 
                 <pre>{{ $project->public_key }}</pre>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-right" data-dismiss="modal">{!! Lang::get('app.close') !!}</button>
+                <button type="button" class="btn btn-default pull-right" data-dismiss="modal">{{ Lang::get('app.close') }}</button>
             </div>
         </div>
     </div>
