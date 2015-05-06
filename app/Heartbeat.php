89db2f5f77b74c718ce4b354107571124482641c
Stephen Ball <stephen@rebelinblue.com>
2015-05-06 16:02:31 +0100

    Starting to add heartbeat model

---
+++ app/Heartbeat.php
@@ -0,0 +1,75 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+
+class Heartbeat extends Model
+{
+    use SoftDeletes;
+
+    const OK         = 0;
+    const UNTESTED   = 1;
+    const MISSING    = 2;
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['project_id', 'created_at', 'updated_at', 'deleted_at', 'pivot'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'interval', 'project_id'];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'status'      => 'integer',
+        'deploy_code' => 'boolean'
+    ];
+
+    /**
+     * Belongs to relationship
+     *
+     * @return Project
+     */
+    public function project()
+    {
+        return $this->belongsTo('App\Project');
+    }
+
+    /**
+     * Override the boot method to bind model event listeners
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        // When first creating the model generate a webhook hash
+        static::creating(function ($model) {
+            if (!array_key_exists('hash', $model->attributes)) {
+                $model->generateHash();
+            }
+        });
+    }
+
+    /**
+     * Generates a hash for use in the webhook URL
+     *
+     * @return void
+     */
+    public function generateHash()
+    {
+        $this->attributes['hash'] = Str::random(60);
+    }
+}
