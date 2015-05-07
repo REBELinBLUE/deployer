0ef687a332a07f5b01222d815430fc328cb51e14
Stephen Ball <stephen@rebelinblue.com>
2015-05-07 15:09:19 +0000

    Changing the output column type from text to longtext

---
+++ database/migrations/2015_05_07_145127_change_output_column.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\ServerLog;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeOutputColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('server_logs', function (Blueprint $table) {
+            $table->longtext('new_output')->nullable();
+        });
+
+        foreach(ServerLog::all() as $log) {
+            $log->new_output = $log->output;
+            $log->save();
+        }
+
+        Schema::table('server_logs', function (Blueprint $table) {
+            $table->dropColumn('output');
+        });
+
+        Schema::table('server_logs', function (Blueprint $table) {
+            $table->renameColumn('new_output', 'output');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('server_logs', function (Blueprint $table) {
+            $table->text('new_output')->nullable();
+        });
+
+        foreach(ServerLog::all() as $log) {
+            $log->new_output = $log->output;
+            $log->save();
+        }
+
+        Schema::table('server_logs', function (Blueprint $table) {
+            $table->dropColumn('output');
+        });
+
+        Schema::table('server_logs', function (Blueprint $table) {
+            $table->renameColumn('new_output', 'output');
+        });
+    }
+}
