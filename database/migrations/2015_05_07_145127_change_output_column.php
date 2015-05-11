ab35e9763e9c768b94a58077f32e4333121367ee
Stephen Ball <stephen@aardvarklondon.com>
2015-05-11 12:44:18 +0100

    Fixing an issue with the migration not working due to there being an enum column

--- database/migrations/2015_05_07_145127_change_output_column.php
+++ database/migrations/2015_05_07_145127_change_output_column.php
@@ -4,7 +4,7 @@ use App\ServerLog;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-// TODO: Test this migration on mysql as it seems to be acting strangely
+// FIXME: Prevent this migration from causing errors on SQLite
 class ChangeOutputColumn extends Migration
 {
     /**
@@ -14,22 +14,9 @@ class ChangeOutputColumn extends Migration
      */
     public function up()
     {
-        Schema::table('server_logs', function (Blueprint $table) {
-            $table->longtext('new_output')->nullable();
-        });
-
-        foreach(ServerLog::all() as $log) {
-            $log->new_output = $log->output;
-            $log->save();
+        if ($_ENV['DB_TYPE'] != 'sqlite') {
+            DB::statement('ALTER TABLE server_logs CHANGE output output longtext');
         }
-
-        Schema::table('server_logs', function (Blueprint $table) {
-            $table->dropColumn('output');
-        });
-
-        Schema::table('server_logs', function (Blueprint $table) {
-            $table->renameColumn('new_output', 'output');
-        });
     }
 
     /**
@@ -39,21 +26,8 @@ class ChangeOutputColumn extends Migration
      */
     public function down()
     {
-        Schema::table('server_logs', function (Blueprint $table) {
-            $table->text('new_output')->nullable();
-        });
-
-        foreach(ServerLog::all() as $log) {
-            $log->new_output = $log->output;
-            $log->save();
+        if ($_ENV['DB_TYPE'] != 'sqlite') {
+            DB::statement('ALTER TABLE server_logs CHANGE output output text');
         }
-
-        Schema::table('server_logs', function (Blueprint $table) {
-            $table->dropColumn('output');
-        });
-
-        Schema::table('server_logs', function (Blueprint $table) {
-            $table->renameColumn('new_output', 'output');
-        });
     }
 }
