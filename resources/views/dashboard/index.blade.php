62b03753e45c1d59c0e5351fadebd2c373b546ee
Stephen Ball <stephen@aardvarklondon.com>
2015-05-13 14:07:58 +0100

    HTML fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -77,7 +77,7 @@
                                     <span class="time"><i class="fa fa-clock-o"></i> {{ $deployment->started_at->format('H:i') }}</span>
                                     <h3 class="timeline-header"><a href="{{ url('projects', $deployment->project_id) }}">{{ $deployment->project->name }} </a> - <a href="{{ route('deployment', $deployment->id) }}">{{ Lang::get('dashboard.deployment_num', ['id' => $deployment->id]) }}</a> - {{ $deployment->readable_status }}</h3>
                                     <div class="timeline-body">
-                                        <a href="{{ $deployment->branchURL() }}" target="_blank">span class="label label-default">{{ $deployment->branch }}</span></a> - {{ $deployment->reason }}
+                                        <a href="{{ $deployment->branchURL() }}" target="_blank"><span class="label label-default">{{ $deployment->branch }}</span></a> - {{ $deployment->reason }}
                                     </div>
                                 </div>
                             </li>
