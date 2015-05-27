7d2e92984c4a4e6286dbde122c1a29994112dfb5
iflamed <iflamed@gmail.com>
2015-05-28 01:02:45 +0800

    add the check urls associate language pack

---
+++ resources/lang/en/checkUrls.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'label'              => 'Check Links',
+    'create'             => 'Add a new url',
+    'edit'               => 'Edit the url',
+    'none'               => 'You can add urls which should be requested periodically after a deployment finished.',
+    'title'              => 'Info',
+    'url'                => 'URL',
+    'period'             => 'Period (min)',
+    'is_report'          => 'Report'
+
+];
