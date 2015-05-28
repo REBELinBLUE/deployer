eb8ed84c482e36e0259e8cfd65ee5739998036e8
Stephen Ball <stephen@aardvarklondon.com>
2015-05-28 13:44:03 +0100

    Small tweak to date when there is not deployment yet

--- resources/views/cctray.blade.php
+++ resources/views/cctray.blade.php
@@ -3,9 +3,9 @@
     @foreach ($projects as $project)
 
         @if (!is_null($project->latest_deployment)) 
-            <Project name="{{ Lang::get('app.name') }}: {{ $project->group->name }} / {{ $project->name }}" activity="{{ $project->cc_tray_status }}" lastBuildLabel="{{ $project->latest_deployment->id }}" lastBuildStatus="{{ $project->latest_deployment->cc_tray_status }}" lastBuildTime="{{ $project->last_run ? $project->last_run->toW3cString() : 'Never' }}" webUrl="{{ url('deployment', $project->latest_deployment->id) }}" />
+            <Project name="{{ Lang::get('app.name') }}: {{ $project->group->name }} / {{ $project->name }}" activity="{{ $project->cc_tray_status }}" lastBuildLabel="{{ $project->latest_deployment->id }}" lastBuildStatus="{{ $project->latest_deployment->cc_tray_status }}" lastBuildTime="{{ $project->last_run->toW3cString() }}" webUrl="{{ url('deployment', $project->latest_deployment->id) }}" />
         @else
-            <Project name="{{ Lang::get('app.name') }}: {{ $project->group->name }} / {{ $project->name }}" activity="{{ $project->cc_tray_status }}" lastBuildTime="Never" webUrl="{{ url('projects', $project->id) }}" />
+            <Project name="{{ Lang::get('app.name') }}: {{ $project->group->name }} / {{ $project->name }}" activity="{{ $project->cc_tray_status }}" lastBuildTime="" webUrl="{{ url('projects', $project->id) }}" />
         @endif
 
     @endforeach
