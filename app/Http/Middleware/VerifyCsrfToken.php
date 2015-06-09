46377a73a86d9103994a01f2b2d16764cba6f070
Stephen Ball <stephen@aardvarklondon.com>
2015-06-09 17:55:08 +0100

    Cleaning up middleware for 5.1

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,48 +1,17 @@
-<?php namespace App\Http\Middleware;
+<?php
 
-use Closure;
+namespace App\Http\Middleware;
+
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
-use Illuminate\Http\Request;
 
-/**
- * Middleware to prevent CSRF
- */
 class VerifyCsrfToken extends BaseVerifier
 {
     /**
-     * Handle an incoming request.
+     * The URIs that should be excluded from CSRF verification.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
+     * @var array
      */
-    public function handle($request, Closure $next)
-    {
-        if ($this->excludedRoutes($request)) {
-            return $this->addCookieToResponse($request, $next($request));
-        }
-
-        return parent::handle($request, $next);
-    }
-
-    /**
-     * Determines whether or not the request should be excluded from CSRF protection
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return boolean
-     */
-    protected function excludedRoutes(Request $request)
-    {
-        $routes = [
-            'deploy/*'
-        ];
-
-        foreach ($routes as $route) {
-            if ($request->is($route)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
+    protected $except = [
+        'deploy/*'
+    ];
 }
