969fb271125fd44df71c4ca912b53d689f8a6798
Stephen Ball <stephen@rebelinblue.com>
2015-06-09 20:48:59 +0100

    Remove unneeded register functions

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -9,17 +9,6 @@ use Illuminate\Foundation\Auth\ResetsPasswords;
  */
 class PasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
-
     use ResetsPasswords;
 
     /**
