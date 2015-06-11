a4e5c99ce4c05273a17eb20933c9cde4d8adeb71
Stephen Ball <stephen@rebelinblue.com>
2015-06-11 21:22:14 +0100

    Tweaking statuses for links

--- resources/assets/js/checkUrls.js
+++ resources/assets/js/checkUrls.js
@@ -189,6 +189,8 @@ var app = app || {};
                 data.status     = Lang.CheckUrls.success;
             }
 
+            data.interval_label = data.period + ' ' + Lang.CheckUrls.length;
+
             // data.report = Lang.CheckUrls.no;
 
             // if (data.is_report) {
