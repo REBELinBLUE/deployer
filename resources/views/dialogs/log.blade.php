621ddd128ee152e26f1fef98aadf4be30e35856d
Stephen Ball <stephen@rebelinblue.com>
2015-06-17 23:05:42 +0100

    Changing icon for logs

--- resources/views/dialogs/log.blade.php
+++ resources/views/dialogs/log.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                <h4 class="modal-title"><i class="fa fa-copy"></i> {{ Lang::get('deployments.process') }} (<span id="action">&nbsp;</span>)</h4>
+                <h4 class="modal-title"><i class="fa fa-file-o"></i> {{ Lang::get('deployments.process') }} (<span id="action">&nbsp;</span>)</h4>
             </div>
             <div class="modal-body">
                 <div id="loading">
