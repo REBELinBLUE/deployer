a46b3c775feab10df6516779a8334be42d88af79
Stephen Ball <stephen@aardvarklondon.com>
2015-06-19 16:35:36 +0100

    Shows whether the command is optional

--- resources/views/commands/listing.blade.php
+++ resources/views/commands/listing.blade.php
@@ -12,6 +12,13 @@
         <td data-command-id="<%- id %>"><%- name %></td>
         <td><%- user %></td>
         <td>
+            <% if (optional) { %>
+                {{ Lang::get('app.yes') }}
+            <% } else { %>
+                {{ Lang::get('app.no') }}
+            <% } %>
+        </td>
+        <td>
             <div class="btn-group pull-right">
                 <button type="button" class="btn btn-default btn-edit" title="{{ Lang::get('commands.edit') }}" data-toggle="modal" data-target="#command"><i class="fa fa-edit"></i></button>
             </div>
