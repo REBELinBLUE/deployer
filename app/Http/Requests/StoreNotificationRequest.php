f2f32c1905119d993b17e5e3b4bb85994bf4da61
Stephen Ball <stephen@rebelinblue.com>
2015-06-20 21:56:51 +0100

    Validate slack URL and channel name

--- app/Http/Requests/StoreNotificationRequest.php
+++ app/Http/Requests/StoreNotificationRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
+use Illuminate\Validation\Factory;
 
 /**
  * Request for validating notifications.
@@ -10,17 +11,34 @@ use App\Http\Requests\Request;
 class StoreNotificationRequest extends Request
 {
     /**
+     * Overwrite the parent constructor to define a new validator.
+     *
+     * @param  Factory $factory
+     * @return void
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    public function __construct(Factory $factory)
+    {
+        $factory->extend(
+            'channel',
+            function ($attribute, $value, $parameters) {
+                $first_character = substr($value, 0, 1);
+                return (($first_character === '#' || $first_character === '@') && strlen($value) > 1);
+            }
+        );
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
     public function rules()
     {
-        // FIXME: Check channel and webhook are valid
         return [
             'name'         => 'required|max:255',
-            'channel'      => 'required|max:255',
-            'webhook'      => 'required|url',
+            'channel'      => 'required|max:255|channel',
+            'webhook'      => 'required|regex:/^https:\/\/hooks.slack.com\/services\/[a-z0-9]+\/[a-z0-9]+\/[a-z0-9]+$/i',
             'failure_only' => 'boolean',
             'project_id'   => 'required|integer|exists:projects,id',
         ];
