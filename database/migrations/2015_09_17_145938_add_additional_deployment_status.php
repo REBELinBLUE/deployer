4b7c234adc960c58499e7a1df7e5de2dc282556e
Stephen Ball <stephen@rebelinblue.com>
2015-09-17 15:13:57 +0100

    DB migration to add the additional status to deployments, horrible!

---
+++ database/migrations/2015_09_17_145938_add_additional_deployment_status.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use REBELinBLUE\Deployer\Deployment;
+
+/**
+ * @TODO Figure out a better way to do this!
+ */
+class AddAdditionalDeploymentStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        if (isset($_ENV['DB_TYPE']) && $_ENV['DB_TYPE'] !== 'sqlite') {
+            DB::statement("ALTER TABLE deployments CHANGE status status ENUM('"
+                . Deployment::PENDING . "', '"
+                . Deployment::DEPLOYING . "', '"
+                . Deployment::COMPLETED . "', '"
+                . Deployment::FAILED . "', '"
+                . Deployment::COMPLETED_WITH_ERRORS . "') DEFAULT '" . Deployment::PENDING . "'");
+        }
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (isset($_ENV['DB_TYPE']) && $_ENV['DB_TYPE'] !== 'sqlite') {
+            DB::statement("ALTER TABLE deployments CHANGE status status ENUM('"
+                . Deployment::PENDING . "', '"
+                . Deployment::DEPLOYING . "', '"
+                . Deployment::COMPLETED . "', '"
+                . Deployment::FAILED . "') DEFAULT '" . Deployment::PENDING . "'");
+        }
+    }
+}
