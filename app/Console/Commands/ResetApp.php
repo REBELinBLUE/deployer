f4bad507bec57182bb632861899b7defeffa071a
Stephen Ball <stephen@rebelinblue.com>
2015-10-03 16:24:05 +0100

    Command to reset DB

--- app/Console/Commands/ResetApp.php
+++ app/Console/Commands/ResetApp.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 /**
  * A console command for clearing all data and setting up again.
  */
-class ResetApp extends Command
+class ResetApp extends InstallApp
 {
     /**
      * The name and signature of the console command.
@@ -40,6 +40,40 @@ class ResetApp extends Command
      */
     public function handle()
     {
-        //
+        if (!$this->verifyNotProduction()) {
+            return;
+        }
+
+        $this->resetDB();
+        $this->migrate();
+        $this->optimize();
+    }
+
+    protected function resetDB()
+    {
+        $this->info('Resetting the database');
+        $this->line('');
+        $this->call('migrate:reset', ['--force' => true]);
+        $this->line('');
+    }
+
+    /**
+     * Ensures that the command is running locally and in debugging mode.
+     * 
+     * @return boolean
+     */
+    private function verifyNotProduction()
+    {
+        if ($this->getLaravel()->environment() !== 'local') {
+            $this->block([
+                'Deployer is not in development mode!',
+                PHP_EOL,
+                'This command does not run in production as its purpose is to wipe your database'
+            ]);
+
+            return false;
+        }
+
+        return true;
     }
 }
