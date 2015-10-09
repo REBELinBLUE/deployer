547386d91ff2174b44f71570d166535a15d6c65f
Stephen Ball <stephen@rebelinblue.com>
2015-10-09 17:06:03 +0000

    Fix an install with the installer due to the cache created by composer

--- app/Console/Commands/InstallApp.php
+++ app/Console/Commands/InstallApp.php
@@ -63,6 +63,8 @@ class InstallApp extends Command
             return;
         }
 
+        $this->clearCaches();
+
         // TODO: Add options so they can be passed in via the command line?
 
         $config = base_path('.env');
