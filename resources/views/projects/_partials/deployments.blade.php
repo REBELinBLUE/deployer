637bf8a5e43aad2eb18feb5cbda2574215fa9732
Stephen Ball <stephen@rebelinblue.com>
2016-01-06 00:38:15 +0000

    Added the abort link

--- resources/views/projects/_partials/deployments.blade.php
+++ resources/views/projects/_partials/deployments.blade.php
@@ -57,7 +57,7 @@
                             <button type="button" class="btn btn-default btn-redeploy" title="{{ Lang::get('deployments.reactivate') }}"><i class="fa fa-cloud-upload"></i></button>
                             @endif
                             @if($deployment->isPending() || $deployment->isRunning())
-                            <button type="button" class="btn btn-default btn-cancel" title="{{ Lang::get('deployments.cancel') }}"><i class="fa fa-ban"></i></button>
+                            <a href="{{ route('abort', ['id' => $deployment->id])  }}" class="btn btn-default btn-cancel" title="{{ Lang::get('deployments.cancel') }}"><i class="fa fa-ban"></i></a>
                             @endif
                             <a href="{{ route('deployment', ['id' => $deployment->id]) }}" type="button" class="btn btn-default" title="{{ Lang::get('app.details') }}"><i class="fa fa-info-circle"></i></a>
                         </div>
