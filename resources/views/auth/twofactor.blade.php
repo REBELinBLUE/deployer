c83f21a98d5e40d8edaf14a3104cc1d4b92d449b
Stephen Ball <stephen@rebelinblue.com>
2016-01-10 17:48:42 +0000

    Tidy up 2FA screen

--- resources/views/auth/twofactor.blade.php
+++ resources/views/auth/twofactor.blade.php
@@ -5,26 +5,16 @@
         <div class="login-logo">
             <b>{{ Lang::get('app.name') }}</b>
         </div>
-        @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <strong>{{ Lang::get('auth.oops') }}</strong> {{ Lang::get('auth.problems') }}<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
         <div class="login-box-body">
-            <p class="login-box-msg">2fa</p>
+            <p class="login-box-msg">Please enter your authentication code.</p>
             <form action="{{ route('two-factor') }}" method="post">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}" />
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="cpde" name="2fa_code" required />
+                    <input type="text" class="form-control" placeholder="Authentication Code" name="2fa_code" required />
                 </div>
                 <div class="row">
                     <div class="col-xs-6">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">validate</button>
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">{{ Lang::get('auth.sign_in') }}</button>
                     </div>
                 </div>
             </form>
