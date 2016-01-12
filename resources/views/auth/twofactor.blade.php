85bb2a1fe096f98e87cc44e3f359fcdf25c11c58
Stephen Ball <stephen@rebelinblue.com>
2016-01-12 20:17:00 +0000

    Added an icon to the auth code input

--- resources/views/auth/twofactor.blade.php
+++ resources/views/auth/twofactor.blade.php
@@ -10,8 +10,9 @@
             <p class="login-box-msg">{{ Lang::get('auth.please_enter_code') }}</p>
             <form action="{{ route('auth.twofactor') }}" method="post">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="{{ Lang::get('auth.authentication_code') }}" name="2fa_code" required />
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" maxlength="6" placeholder="{{ Lang::get('auth.authentication_code') }}" name="2fa_code" required />
+                    <span class="fa fa-user-secret form-control-feedback"></span>
                 </div>
                 <div class="row">
                     <div class="col-xs-6">
