42e21e35ad04fd99617bac9686b3d2dbcd5b35f3
Stephen Ball <stephen@nudgecrm.com>
2016-01-13 14:05:41 +0000

    Clean up the update message

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -29,7 +29,7 @@ return [
                            'for reporting the status on running deployments. Please reload, if the issue continues please contact the system administrator',
     'not_down'          => 'You must switch to maintenance mode before running this command, this will ensure that no new deployments are started',
     'switch_down'       => 'Switch to maintenance mode now? The app will switch back to live mode once cleanup is finished',
-    'version'           => 'Version',
-    'outdated'          => 'You are running an out of date release, there is an update (<a href=":link">:version</a>) available!',
+    'update_available'  => 'An update is available!',
+    'outdated'          => 'You are running an out of date release :current, there is an updated release <a href=":link">:latest</a> available!',
 
 ];
