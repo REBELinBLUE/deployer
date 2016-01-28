5870663a23f28d8de55850259af3a02ce38e6abd
Stephen Ball <stephen@nudgecrm.com>
2016-01-28 11:14:45 +0000

    Updated build script to not include JS/CSS map files in production build and correctly install the NPM dev utilities as they are needed to run gulp

--- build_release.sh
+++ build_release.sh
@@ -15,15 +15,14 @@ git commit -m "Incrementing version"
 
 git rm -rf public/build/*
 rm -rf public/build/
-npm install --production
-composer install --no-dev -o
-bower install
+npm install
+composer install
 gulp --production
 
 git add -f public/build/rev-manifest.json
-git add -f public/build/css/*
-git add -f public/build/js/*
-git add -f public/build/*
+git add -f public/build/css/*.css
+git add -f public/build/js/*.js
+git add -f public/build/fonts/*
 git commit -am "Updating assets"
 git push
 
