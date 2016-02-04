222abeffce37690fb72e5d8868d7a5919a60bd87
Stephen Ball <stephen@rebelinblue.com>
2016-02-04 16:55:58 +0000

    Fixed typo

--- app/Message.php
+++ app/Message.php
@@ -1,11 +1,19 @@
 <?php
 
-namespace REBELinBLUE\Deployer
+namespace REBELinBLUE\Deployer;
 
 /**
  * Generic class for holding messages
  */
 class Message
 {
+    public function setMessage()
+    {
 
+    }
+
+    public function addField()
+    {
+
+    }
 }
