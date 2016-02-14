379be67d5f5e8d183e8b11e2ded0fc6b00e834f3
Stephen Ball <stephen@rebelinblue.com>
2016-02-14 19:13:31 +0000

    Remove orphaned mirror directories

--- app/Console/Commands/ClearOrphanMirrors.php
+++ app/Console/Commands/ClearOrphanMirrors.php
@@ -3,6 +3,8 @@
 namespace REBELinBLUE\Deployer\Console\Commands;
 
 use Illuminate\Console\Command;
+use REBELinBLUE\Deployer\Project;
+use Symfony\Component\Process\Process;
 
 /**
  * Checks for and cleans up orphaned git mirrors.
@@ -40,6 +42,36 @@ class ClearOrphanMirrors extends Command
      */
     public function handle()
     {
-        //
+        $current_mirrors = [];
+
+        Project::where('is_template', false)->chunk(10, function ($projects) use (&$current_mirrors) {
+            foreach ($projects as $project) {
+                $current_mirrors[] = $project->mirrorPath();
+            }
+        });
+
+        $current_mirrors = collect($current_mirrors);
+
+        $all_mirrors = collect(glob(storage_path('app/mirrors/') . '*.git'));
+
+        // Compare the 2 collections get a list of mirrors which are no longer in use
+        $orphan_mirrors = $all_mirrors->diff($current_mirrors);
+
+        $this->info('Found ' . $orphan_mirrors->count() . ' orphaned mirrors');
+
+        // Now loop through the mirrors and delete them from storage
+        foreach ($orphan_mirrors as $mirror_dir) {
+
+            $process = new Process("rm -rf {$mirror_dir}");
+            $process->setTimeout(null);
+            $process->run();
+
+            if ($process->isSuccessful()) {
+                $this->info('Deleted ' . basename($mirror_dir));
+            } else {
+                $this->info('Failed to delete ' . basename($mirror_dir));
+            }
+        }
+
     }
 }
