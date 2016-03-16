39212ea41a10d2f44c796321180e11d6087561c6
Stephen Ball <stephen@rebelinblue.com>
2016-03-16 20:34:30 +0000

    Changed language string

--- resources/lang/en/projects.php
+++ resources/lang/en/projects.php
@@ -35,7 +35,7 @@ return [
     'redeploy'          => 'Redeploy',
     'server_keys'       => 'This key must be added to the server\'s <code>~/.ssh/authorized_keys</code> ' .
                            'for each user you wish to run commands as.',
-    'gitlab_keys'       => 'The key will also need to be added to the <strong>Deploy Keys</strong> ' .
+    'git_keys'          => 'The key will also need to be added to the <strong>Deploy Keys</strong> ' .
                            'for you repository unless you are using a public/unautheticated URL.',
     'finished'          => 'Finished',
     'pending'           => 'Pending',
