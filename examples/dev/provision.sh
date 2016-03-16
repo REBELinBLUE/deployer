5450e740f7ed55e568198f073cd96372be4c7b76
Stephen Ball <stephen@rebelinblue.com>
2016-03-16 13:14:02 +0000

    Re-added drop statement

--- examples/dev/provision.sh
+++ examples/dev/provision.sh
@@ -52,6 +52,7 @@ apt-get install ruby ruby-dev -y
 gem install github_changelog_generator -v 1.11.3
 
 # Create DB
+mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS deployer;"
 mysql -uhomestead -psecret -e "CREATE DATABASE deployer DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci;"
 
 # Install JS CS, redis commander & diff-so-fancy
