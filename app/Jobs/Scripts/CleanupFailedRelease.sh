6158a6af414fd9bc17c6252c6827791f01b282f2
Stephen Ball <stephen@nudgecrm.com>
2016-03-17 10:13:08 +0000

    Revert "Move cleanup to a shell script"
    This reverts commit a8a10d2d03b1c3fd6cf6e301cec7206eea71c6c3.

--- app/Jobs/Scripts/CleanupFailedRelease.sh
+++
@@ -1,7 +0,0 @@
-cd {{ project_path }}
-
-# Remove the archive
-[ -f {{ remote_archive }} ] && rm {{ remote_archive }}
-
-# Remove the release directory
-[ -d {{ release_path }} ] && rm -rf {{ release_path }}
