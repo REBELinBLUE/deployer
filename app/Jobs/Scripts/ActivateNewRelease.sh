e1a9c3284439332738f2b4685a84043872f51983
Stephen Ball <stephen@nudgecrm.com>
2016-03-17 10:25:05 +0000

    Revert "Refactoring deployment to move commands to separate command files"
    This reverts commit f7ad762a460efd46170e084528d48e4b109b2c33.

--- app/Jobs/Scripts/ActivateNewRelease.sh
+++
@@ -1,3 +0,0 @@
-cd {{ project_path }}
-[ -h {{ project_path }}/latest ] && rm {{ project_path }}/latest
-ln -s {{ release_path }} {{ project_path }}/latest
--- app/Jobs/Scripts/CreateNewRelease.sh
+++
@@ -1,8 +0,0 @@
-cd {{ project_path }}
-[ ! -d {{ releases_path }} ] && mkdir {{ releases_path }}
-[ ! -d {{ shared_path }} ] && mkdir {{ shared_path }}
-mkdir {{ release_path }}
-cd {{ release_path }}
-echo -e "\nExtracting...\n"
-tar --warning=no-timestamp --gunzip --verbose --extract --file={{ remote_archive }} --directory={{ release_path }}
-rm {{ remote_archive }}
