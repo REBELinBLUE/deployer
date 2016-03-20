75d5eaa61eadf790a10d23184ae54c8547d63be5
Stephen Ball <stephen@rebelinblue.com>
2016-03-20 11:29:46 +0000

    Fixed mirroring script

--- resources/scripts/tools/MirrorGitRepository.sh
+++ resources/scripts/tools/MirrorGitRepository.sh
@@ -1,10 +1,10 @@
-cd {{ mirror_path }}
-
 chmod +x "{{ wrapper_file }}"
 export GIT_SSH="{{ wrapper_file }}"
 
 if [ ! -d {{ mirror_path }} ]; then
     git clone --mirror {{ repository }} {{ mirror_path }}
 fi
+
+cd {{ mirror_path }}
 
 git fetch --all --prune
