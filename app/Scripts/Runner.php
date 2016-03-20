17f8f54b83bb4bff9d94583979e5e26fd697082d
Stephen Ball <stephen@rebelinblue.com>
2016-03-20 20:22:21 +0000

    Docblocks

--- app/Scripts/Runner.php
+++ app/Scripts/Runner.php
@@ -2,9 +2,24 @@
 
 namespace REBELinBLUE\Deployer\Scripts;
 
+use Symfony\Component\Process\Process;
+
 /**
  * Class which runs scripts.
  */
 class Runner
 {
+    /**
+     * Runs a script locally.
+     */
+    public function local()
+    {
+    }
+
+    /**
+     * Runs a script remotely.
+     */
+    public function remote()
+    {
+    }
 }
