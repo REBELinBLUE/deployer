a30f5a0f1ea88243cfdec9915e54684df667a11d
Stephen Ball <stephen@rebelinblue.com>
2016-03-24 16:46:46 +0000

    Fix :channel instead of :attribute in channel validation message

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -28,7 +28,7 @@ return [
         "array"   => "The :attribute must have between :min and :max items.",
     ],
     "boolean"              => "The :attribute field must be true or false.",
-    "channel"              => "The :channel field is not a valid channel.",
+    "channel"              => "The :attribute field is not a valid channel.",
     "confirmed"            => "The :attribute confirmation does not match.",
     "date"                 => "The :attribute is not a valid date.",
     "date_format"          => "The :attribute does not match the format :format.",
