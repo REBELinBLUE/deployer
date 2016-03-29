73350da14f8ccc1f018d2a54157abfbe638ad8a4
Stephen Ball <stephen@rebelinblue.com>
2016-03-29 10:48:17 +0100

    Stop the update link from sending the referrer

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -32,6 +32,6 @@ return [
     'not_down'          => 'You must switch to maintenance mode before running this command, this will ensure that no new deployments are started',
     'switch_down'       => 'Switch to maintenance mode now? The app will switch back to live mode once cleanup is finished',
     'update_available'  => 'An update is available!',
-    'outdated'          => 'You are running an out of date release :current, there is an updated release <a href=":link">:latest</a> available!',
+    'outdated'          => 'You are running an out of date release :current, there is an updated release <a href=":link" rel="noreferrer">:latest</a> available!',
 
 ];
