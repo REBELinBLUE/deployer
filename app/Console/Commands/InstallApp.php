ac7ec1bc1cb4f82f2be843528548ae0665ac6a17
Stephen Ball <stephen@rebelinblue.com>
2016-04-06 12:23:45 +0100

    Check permissions of mirror directory

--- app/Console/Commands/InstallApp.php
+++ app/Console/Commands/InstallApp.php
@@ -616,8 +616,8 @@ class InstallApp extends Command
         }
 
         // Files and directories which need to be writable
-        $writable = ['.env', 'storage', 'storage/logs', 'storage/app', 'storage/framework',
-                     'storage/framework/cache', 'storage/framework/sessions',
+        $writable = ['.env', 'storage', 'storage/logs', 'storage/app', 'storage/app/mirrors',
+                     'storage/framework', 'storage/framework/cache', 'storage/framework/sessions',
                      'storage/framework/views', 'bootstrap/cache', 'public/upload',
                     ];
 
