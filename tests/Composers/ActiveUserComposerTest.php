c8e3b9119b3e8125a15d4129eef99291cad97af9
Stephen Ball <stephen@rebelinblue.com>
2016-05-13 21:59:06 +0100

    Active user composer test

---
+++ tests/Composers/ActiveUserComposerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use REBELinBLUE\Deployer\Composers\ActiveUserComposer;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+
+class ActiveUserComposerTest extends TestCase
+{
+    public function testCompose()
+    {
+        $expected_user = 123456;
+
+        Auth::shouldReceive('user')
+            ->once()
+            ->andReturn($expected_user);
+
+
+        $view = $this->getMockBuilder(View::class)
+                     ->disableOriginalConstructor()
+                     ->getMock();
+
+        $view->expects($this->any())
+             ->method('with')
+             ->withConsecutive(
+                 [$this->equalTo('logged_in_user'), $this->equalTo($expected_user)]
+             );
+
+        $composer = new ActiveUserComposer;
+        $composer->compose($view);
+    }
+}
