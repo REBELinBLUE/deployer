3c11a62ef1d426418d7c846860c18f48465a3b87
Stephen Ball <REBELinBLUE@users.noreply.github.com>
2016-06-07 20:23:52 +0100

    Update WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -147,8 +147,7 @@ class WebhookController extends Controller
         }
 
         // If the webhook is allowed to deploy other branches check for update only
-        if ($project->allow_other_branch)
-        {
+        if ($project->allow_other_branch) {
             // Check if the request has an update_only query string and if so check the branch matches
             if ($request->has('update_only') && $request->get('update_only') !== false) {
                 $deployment = $this->deploymentRepository->getLatestSuccessful($project->id);
