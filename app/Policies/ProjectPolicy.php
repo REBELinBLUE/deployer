85f70ca35a6afb0632cac0c4ba4ba2750486ee4d
Stephen Ball <stephen@rebelinblue.com>
2016-09-08 23:32:10 +0100

    Added docblocks

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -6,10 +6,20 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 use REBELinBLUE\Deployer\Project;
 use REBELinBLUE\Deployer\User;
 
+/**
+ * Policy for projects
+ */
 class ProjectPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * View permission.
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
+     */
     public function view(User $user, Project $project)
     {
         if ($user->hasPermissionTo('projects.*.view', $project->id)) {
@@ -17,11 +27,25 @@ class ProjectPolicy
         }
     }
 
+    /**
+     * Deploy permission.
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
+     */
     public function deploy(User $user, Project $project)
     {
         return true;
     }
 
+    /**
+     * Manage permission.
+     *
+     * @param User $user
+     * @param Project $project
+     * @return bool
+     */
     public function manage(User $user, Project $project)
     {
         return true;
