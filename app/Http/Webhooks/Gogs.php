5be17158f221d34a05773e1b999e0ecf587bb8f1
axeloz <axel@mabox.eu>
2016-11-24 19:42:34 +0100

    Removing mentions of Github
    Pushed too fast earlier, let mentions of Github in comments, dirty work. Now fixed

--- app/Http/Webhooks/Gogs.php
+++ app/Http/Webhooks/Gogs.php
@@ -3,12 +3,12 @@
 namespace REBELinBLUE\Deployer\Http\Webhooks;
 
 /**
- * Class to handle integration with Github webhooks.
+ * Class to handle integration with Gogs/Gitea webhooks.
  */
 class Gogs extends Webhook
 {
     /**
-     * Determines whether the request was from Github.
+     * Determines whether the request was from Gogs/Gitea.
      *
      * @return bool
      */
