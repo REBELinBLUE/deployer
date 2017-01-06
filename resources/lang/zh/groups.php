39de8ef8a72511fe76fd6fe02341f7b09e7b382c
aiddroid <aiddroid@gmail.com>
2017-01-06 13:59:44 +0800

    fix:fix some chinese words

--- resources/lang/zh/groups.php
+++ resources/lang/zh/groups.php
@@ -6,7 +6,7 @@ return [
     'name'     => '名称',
     'create'   => '添加新项目分组',
     'edit'     => '编辑项目分组',
-    'projects' => '项目',
+    'projects' => '项目数量',
     'warning'  => '项目分组无法保存，请检查下面的表单',
 
 ];
