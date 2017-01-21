c7be560c9b7054bba7f0cd0b5b510870207583a7
Dmitry Khomutov <poisoncorpsee@gmail.com>
2017-01-21 23:23:21 +0700

    Added Russian language translation

---
+++ resources/lang/ru/notifications.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    // Slack
+    'test_slack_message'      => 'This is a test to ensure the notification is setup correctly, if you ' .
+        'can see this it means it is! :+1:',
+    'test_message'            => 'This is a test to ensure the notification is setup correctly, if you have ' .
+        'received this it means it is!',
+    'test_hipchat_message'    => 'This is a test to ensure the notification is setup correctly, if you ' .
+        'can see this it means it is! (corpsethumb)',
+    'test_subject'            => 'Test Notification',
+
+    // Slack & Hipchat
+    'branch'                  => 'Branch',
+    'project'                 => 'Project',
+    'commit'                  => 'Commit',
+    'committer'               => 'Committer',
+
+    // Email
+    'project_name'            => 'Project name',
+    'deployed_branch'         => 'Deployed branch',
+    'deployment_details'      => 'View the deployment',
+    'project_details'         => 'View the project',
+    'started_at'              => 'Started at',
+    'finished_at'             => 'Finished at',
+    'last_committer'          => 'Last committer',
+    'last_commit'             => 'Last commit',
+    'reason'                  => 'Deployment reason - :reason',
+];
