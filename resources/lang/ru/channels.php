529a04e383215dea3c2d46754655a5ec8e7bce29
Dmitry Khomutov <poisoncorpsee@gmail.com>
2017-02-20 02:46:39 +0700

    Added Russian language translation (#315)

---
+++ resources/lang/ru/channels.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+
+    'label'                      => 'Оповещения',
+    'create'                     => 'Добавить новое оповещение',
+    'create_slack'               => 'Добавить новое оповещение в Slack',
+    'create_hipchat'             => 'Добавить новое оповещение в Hipchat',
+    'create_twilio'              => 'Добавить новое оповещение в Twilio',
+    'create_mail'                => 'Добавить новое оповещение по E-mail',
+    'create_custom'              => 'Добавить новое свой тип оповещения',
+    'edit'                       => 'Редактировать оповещение',
+    'edit_slack'                 => 'Редактировать оповещение в Slack',
+    'edit_hipchat'               => 'Редактировать оповещение в Hipchat',
+    'edit_twilio'                => 'Редактировать оповещение в Twilio',
+    'edit_mail'                  => 'Редактировать оповещение по E-mail',
+    'edit_custom'                => 'Редактировать оповещение свой тип оповещения',
+    'none'                       => 'В проекте сейчас не настроено никаких оповещений',
+    'name'                       => 'Название',
+    'type'                       => 'Тип',
+    'warning'                    => 'Оповещение не может быть сохранено. Пожалуйста, проверьте форму ниже.',
+    'not_configured_title'       => 'Сервис не настроен',
+    'not_configured'             => 'Выбранный тип оповещений не может быть использован, пока он не настроен.',
+    'triggers'                   => 'Триггеры',
+    'deployments'                => 'Сборки',
+    'succeeded'                  => 'Успешна',
+    'failed'                     => 'Провалена',
+    'urls'                       => 'URLы',
+    'down'                       => 'Упал',
+    'still_down'                 => 'Все еще лежит',
+    'recovered'                  => 'Восстановлен',
+    'heartbeats'                 => 'Heartbeats',
+    'missing'                    => 'Пропущена',
+    'still_missing'              => 'Все еще пропущена',
+    'on_deployment_success'      => 'Сборка успешна',
+    'on_deployment_failure'      => 'Сборка провалена',
+    'on_link_down'               => 'URL провален',
+    'on_link_still_down'         => 'URL провален снова',
+    'on_link_recovered'          => 'URL восстановлен',
+    'on_heartbeat_missing'       => 'Heartbeat пропущен',
+    'on_heartbeat_still_missing' => 'Heartbeat все еще пропущен',
+    'on_heartbeat_recovered'     => 'Heartbeat восстановлен',
+    'custom'                     => 'Свой тип',
+    'slack'                      => 'Slack',
+    'hipchat'                    => 'Hipchat',
+    'twilio'                     => 'Twilio',
+    'mail'                       => 'E-mail',
+    'which'                      => 'Какой тип оповещений вы хотели бы добавить?',
+    'webhook'                    => 'Webhook URL',
+
+    // Slack
+    'icon'                       => 'Изображение',
+    'bot'                        => 'Бот',
+    'icon_info'                  => 'Либо emoji, например :ghost:, либо URL-адрес изображения',
+    'channel'                    => 'Канал',
+
+    // Twilio
+    'telephone'                  => 'Телефонный номер',
+
+    // Hipchat
+    'room'                       => 'Комната',
+
+];
