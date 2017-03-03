d8c7dd3564c49de5ef1837a5dabf26641d765ba0
Stephen Ball <stephen@rebelinblue.com>
2017-03-03 20:42:48 +0000

    Added Deployer to the SSH key comment

--- resources/scripts/tools/GenerateSSHKey.sh
+++ resources/scripts/tools/GenerateSSHKey.sh
@@ -3,4 +3,4 @@ if [ -f {{ key_file }} ]; then
     rm -f {{ key_file }}
 fi
 
-ssh-keygen -q -t rsa -b 2048 -f {{ key_file }} -N "" -C "{{ project }}"
+ssh-keygen -q -t rsa -b 2048 -f {{ key_file }} -N "" -C "Deployer - {{ project }}"
