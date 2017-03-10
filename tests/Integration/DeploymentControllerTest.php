6885d587f116eed8b47f5e4de277d664f94c14b3
Stephen Ball <stephen@rebelinblue.com>
2017-03-10 18:06:41 +0000

    More integration tests

--- tests/Integration/DeploymentControllerTest.php
+++ tests/Integration/DeploymentControllerTest.php
@@ -4,12 +4,22 @@ namespace REBELinBLUE\Deployer\Tests\Integration;
 
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Collection;
+use REBELinBLUE\Deployer\Channel;
+use REBELinBLUE\Deployer\CheckUrl;
+use REBELinBLUE\Deployer\Command;
+use REBELinBLUE\Deployer\ConfigFile;
 use REBELinBLUE\Deployer\Deployment;
+use REBELinBLUE\Deployer\Heartbeat;
 use REBELinBLUE\Deployer\Jobs\AbortDeployment;
 use REBELinBLUE\Deployer\Jobs\QueueUpdateGitMirror;
 use REBELinBLUE\Deployer\Project;
+use REBELinBLUE\Deployer\Repositories\Contracts\ProjectRepositoryInterface;
+use REBELinBLUE\Deployer\Server;
 use REBELinBLUE\Deployer\ServerLog;
 use Mockery as m;
+use REBELinBLUE\Deployer\SharedFile;
+use REBELinBLUE\Deployer\Variable;
 use REBELinBLUE\Deployer\View\Presenters\ServerLogPresenter;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -101,8 +111,6 @@ class DeploymentControllerTest extends AuthenticatedTestCase
      */
     public function testLogWithRuntime()
     {
-        $this->withoutEvents();
-
         /** @var ServerLog $log */
         $log = factory(ServerLog::class)->create([
             'started_at' => Carbon::create(2017, 1, 1, 12, 00, 00),
@@ -114,15 +122,99 @@ class DeploymentControllerTest extends AuthenticatedTestCase
                  ->assertJson($log->toArray())
                  ->assertJson(['runtime' => '15 minutes']);
     }
+
     /**
      * @covers ::__construct
      * @covers ::log
      */
     public function testLogReturnsErrorWhenInvalid()
     {
-        $this->withoutEvents();
-
         $response = $this->getJson('/log/9999');
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::project
+     */
+    public function testProject()
+    {
+        $this->withoutEvents();
+        $this->withoutJobs();
+        $this->withoutNotifications();
+
+        $expectedOptional = 2;
+        $expectedAlways = 5;
+
+        /** @var Project $project */
+        $project = factory(Project::class)->create();
+
+        factory(Command::class, $expectedAlways)->create([
+            'target_type' => 'project',
+            'target_id' => $project->id,
+            'optional' => false,
+        ]);
+
+        /** @var Collection $optional */
+        $optional = factory(Command::class, $expectedOptional)->create([
+            'target_type' => 'project',
+            'target_id' => $project->id,
+            'optional' => true,
+        ]);
+
+        $config = [
+            'project_id' => $project->id
+        ];
+
+        $target = [
+            'target_id' => $project->id,
+            'target_type' => 'project',
+        ];
+
+        factory(Server::class)->create($config);
+        factory(Channel::class)->create($config);
+        factory(Heartbeat::class)->create($config);
+        factory(CheckUrl::class)->create($config);
+        factory(SharedFile::class)->create($target);
+        factory(ConfigFile::class)->create($target);
+        factory(Variable::class)->create($target);
+
+        $project = $this->app->make(ProjectRepositoryInterface::class)->getById($project->id);
+
+        $response = $this->get('/projects/1');
+
+        $response->assertStatus(Response::HTTP_OK)->assertViewHas([
+            'target_type', 'target_id', 'optional', 'tags', 'branches', 'servers', 'channels', 'heartbeats',
+            'sharedFiles', 'configFiles', 'checkUrls', 'variables', 'deployments', 'route'
+        ]);
+
+        /** @var \McCool\LaravelAutoPresenter\BasePresenter $view */
+        $view = $response->getOriginalContent();
+
+        $this->assertSame('project', $view->target_type);
+        $this->assertSame($project->id, $view->target_id);
+        $this->assertSame($project->id, $view->project->id);
+        $this->assertSame($project->name, $view->project->name);
+        $this->assertSame($project->branch, $view->project->branch);
+        $this->assertSame($project->allow_other_branch, $view->project->allow_other_branch);
+        $this->assertSame($project->include_dev, $view->project->include_dev);
+        $this->assertSame($project->servers->toJson(), $view->servers->toJson());
+        $this->assertSame($project->channels->toJson(), $view->channels->toJson());
+        $this->assertSame($project->heartbeats->toJson(), $view->heartbeats->toJson());
+        $this->assertSame($project->sharedFiles->toJson(), $view->sharedFiles->toJson());
+        $this->assertSame($project->configFiles->toJson(), $view->configFiles->toJson());
+        $this->assertSame($project->checkUrls->toJson(), $view->checkUrls->toJson());
+        $this->assertSame($project->variables->toJson(), $view->variables->toJson());
+        $this->assertSame($optional->pluck('id')->toArray(), $view->optional->pluck('id')->toArray());
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::project
+     */
+    public function testProjectReturnsErrorWhenInvalid()
+    {
+        $response = $this->get('/projects/9999');
         $response->assertStatus(Response::HTTP_NOT_FOUND);
     }
 }
