8349742e8bde18abc63e955e4068f0c8fc317b64
Stephen Ball <stephen@rebelinblue.com>
2017-03-11 15:30:04 +0000

    More tests for middleware

---
+++ tests/Unit/Http/Middleware/LocaleTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace REBELinBLUE\Deployer\Tests\Unit\Http\Middleware;
+
+use Illuminate\Contracts\Auth\Factory;
+use Illuminate\Http\Request;
+use MicheleAngioni\MultiLanguage\LanguageManager;
+use Mockery as m;
+use REBELinBLUE\Deployer\Tests\TestCase;
+use REBELinBLUE\Deployer\Http\Middleware\Locale;
+
+/**
+ * @coversDefaultClass \REBELinBLUE\Deployer\Http\Middleware\Locale
+ */
+class LocaleTest extends TestCase
+{
+    private $auth;
+    private $manager;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->auth = m::mock(Factory::class);
+        $this->manager = m::mock(LanguageManager::class);
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::handle
+     */
+    public function testHandleNotAuthenticated()
+    {
+        $expectedGuard = 'session';
+
+        $expected = m::mock(Request::class);
+
+        $this->manager->shouldNotReceive('setLocale');
+
+        $closure  = function ($request) use ($expected) {
+            $this->assertSame($request, $expected);
+
+            return true;
+        };
+
+        $this->auth->shouldReceive('guard')->with($expectedGuard)->andReturnSelf();
+        $this->auth->shouldReceive('user')->andReturn(null);
+
+        $middleware = new Locale($this->manager, $this->auth);
+        $actual     = $middleware->handle($expected, $closure, $expectedGuard);
+
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::handle
+     */
+    public function testHandleWhenAuthenticatedWithoutLanguageSet()
+    {
+        $expectedGuard = 'session';
+
+        $expected = m::mock(Request::class);
+
+        $this->manager->shouldNotReceive('setLocale');
+
+        $closure  = function ($request) use ($expected) {
+            $this->assertSame($request, $expected);
+
+            return true;
+        };
+
+        $this->auth->shouldReceive('guard')->with($expectedGuard)->andReturnSelf();
+        $this->auth->shouldReceive('user')->andReturn((object) ['language' => null]);
+
+        $middleware = new Locale($this->manager, $this->auth);
+        $actual     = $middleware->handle($expected, $closure, $expectedGuard);
+
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::handle
+     */
+    public function testHandleWhenAuthenticatedWithLanguageSet()
+    {
+        $expectedGuard = 'session';
+        $expectedLanguage = 'en';
+
+        $expected = m::mock(Request::class);
+
+        $this->manager->shouldReceive('setLocale')->with($expectedLanguage);
+
+        $closure  = function ($request) use ($expected) {
+            $this->assertSame($request, $expected);
+
+            return true;
+        };
+
+        $this->auth->shouldReceive('guard')->with($expectedGuard)->andReturnSelf();
+        $this->auth->shouldReceive('user')->andReturn((object) ['language' => $expectedLanguage]);
+
+        $middleware = new Locale($this->manager, $this->auth);
+        $actual     = $middleware->handle($expected, $closure, $expectedGuard);
+
+        $this->assertTrue($actual);
+    }
+}
---
+++ tests/Unit/Http/Middleware/RedirectIfAuthenticatedTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace REBELinBLUE\Deployer\Tests\Unit\Http\Middleware;
+
+use Illuminate\Contracts\Auth\Factory;
+use Illuminate\Routing\Redirector;
+use Mockery as m;
+use REBELinBLUE\Deployer\Tests\TestCase;
+use REBELinBLUE\Deployer\Http\Middleware\RedirectIfAuthenticated;
+
+/**
+ * @coversDefaultClass \REBELinBLUE\Deployer\Http\Middleware\RedirectIfAuthenticated
+ */
+class RedirectIfAuthenticatedTest extends TestCase
+{
+    private $redirector;
+    private $auth;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->redirector = m::mock(Redirector::class);
+        $this->auth = m::mock(Factory::class);
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::handle
+     */
+    public function testHandleIsNotAuthenticated()
+    {
+        $expectedGuard = 'session';
+
+        $expected = m::mock(Request::class);
+
+        $this->redirector->shouldNotReceive('to');
+
+        $closure  = function ($request) use ($expected) {
+            $this->assertSame($request, $expected);
+
+            return true;
+        };
+
+        $this->auth->shouldReceive('guard')->with($expectedGuard)->andReturnSelf();
+        $this->auth->shouldReceive('check')->andReturn(false);
+
+        $middleware = new RedirectIfAuthenticated($this->redirector, $this->auth);
+        $actual     = $middleware->handle($expected, $closure, $expectedGuard);
+
+        $this->assertTrue($actual);
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::handle
+     */
+    public function testHandleIsAuthenticated()
+    {
+        $expectedGuard = 'session';
+        $expectedRedirect = 'a-redirect-url';
+
+        $expected = m::mock(Request::class);
+
+        $this->redirector->shouldReceive('to')->with(m::type('string'))->andReturn($expectedRedirect);
+
+        $closure  = function () {
+            // Nothing much here, should not execute
+            $this->assertFalse(true);
+        };
+
+        $this->auth->shouldReceive('guard')->with($expectedGuard)->andReturnSelf();
+        $this->auth->shouldReceive('check')->andReturn(true);
+
+        $middleware = new RedirectIfAuthenticated($this->redirector, $this->auth);
+        $actual     = $middleware->handle($expected, $closure, $expectedGuard);
+
+        $this->assertSame($expectedRedirect, $actual);
+    }
+}
