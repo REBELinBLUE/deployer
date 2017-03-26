82f9068f2233468fc0592910f17d8270ce93e5e8
Stephen Ball <stephen@rebelinblue.com>
2017-03-26 15:29:36 +0100

    Updated laravel

--- tests/Unit/Validators/HostValidatorTest.php
+++ tests/Unit/Validators/HostValidatorTest.php
@@ -4,36 +4,26 @@ namespace REBELinBLUE\Deployer\Tests\Unit\Validators;
 
 use REBELinBLUE\Deployer\Tests\TestCase;
 use REBELinBLUE\Deployer\Validators\HostValidator;
+use phpmock\mockery\PHPMockery as phpm;
 
 /**
  * @coversDefaultClass \REBELinBLUE\Deployer\Validators\HostValidator
  */
 class HostValidatorTest extends TestCase
 {
-    protected function setUp()
-    {
-        parent::setUp();
-
-        if (!filter_var(gethostbyname('www.example.com'), FILTER_VALIDATE_IP)) {
-            $this->markTestSkipped('The test can not be run as there was no active network connection');
-        }
-    }
-
     /**
      * @dataProvider validationDataProvider
      * @covers ::validate
      */
-    public function testValidate($value, $valid)
+    public function testValidate($value, $expected, $address)
     {
+        phpm::mock('REBELinBLUE\Deployer\Validators', 'gethostbyname')->andReturn($address);
+
         $validator = new HostValidator();
 
-        $result = $validator->validate('host', $value, null);
+        $actual = $validator->validate('host', $value, null);
 
-        if ($valid) {
-            $this->assertTrue($result);
-        } else {
-            $this->assertFalse($result);
-        }
+        $this->assertSame($expected, $actual);
     }
 
     public function validationDataProvider()
