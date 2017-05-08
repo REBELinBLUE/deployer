5ab00aa4aedc8056de5df655cd1f62ed7dad44a8
Stephen Ball <stephen@rebelinblue.com>
2017-05-08 19:13:02 +0100

    Small updates to match upstream

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Deployer',
+    'name' => env('APP_NAME', 'Deployer'),
 
     /*
     |--------------------------------------------------------------------------
