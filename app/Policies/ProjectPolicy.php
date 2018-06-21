b9467d78670cc578613a621dc9be772319a24b8d
Axel de Vignon <axel@mabox.eu>
2018-06-21 09:51:50 +0200

    Cleaning condition into policy

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -12,7 +12,7 @@ class ProjectPolicy
 
     public function before($user, $ability)
     {
-        if ($user->is_admin === true) {
+        if ($user->isAdmin()) {
             return true;
         }
     }
