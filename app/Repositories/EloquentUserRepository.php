76b236c5ab6660e50b2edcc4a5012c84da0d5692
Axel de Vignon <axel@mabox.eu>
2018-06-21 15:38:36 +0200

    Normalizing coding style

--- app/Repositories/EloquentUserRepository.php
+++ app/Repositories/EloquentUserRepository.php
@@ -72,7 +72,8 @@ class EloquentUserRepository extends EloquentRepository implements UserRepositor
     /**
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function findNonAdminUsers() {
+    public function findNonAdminUsers()
+    {
         return $this->model->where('is_admin', 0)->get();
     }
 
